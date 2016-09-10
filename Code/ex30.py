2462aa99407dce4c0a6e1727e57ed0efe1a8d271
deadking2016 <deadking2006@sina.com>
2016-09-10 02:52:02 -0600

    Create ex30.py

---
+++ Code/ex30.py
@@ -0,0 +1,18 @@
+#ex 30
+people = 30
+cars = 40
+buses = 15
+
+if cars > people: print "We should take cars"
+
+elif cars < people: print "We should not take the car"
+
+else:print "We cannot decide"
+
+if buses > cars:print "That's too many buses"
+
+elif buses < cars:print "Maybe we could take the buses"
+else: print "We still cannot decide"
+
+if people > buses:print "Take the buses"
+else:print "Stay at home"
