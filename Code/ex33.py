40a99f01b870077d01e5094c362dc0571a9f5c9e
deadking2016 <deadking2006@sina.com>
2016-09-10 19:44:10 -0600

    Update ex33.py

--- Code/ex33.py
+++ Code/ex33.py
@@ -1,16 +1,20 @@
 #ex33
-i = 0
-numbers =[]
+def whileTest(limit):
+	i = 0
+	numbers =[]
 
-while i < 6:
-	print "At the top i is %d" %i
-	numbers.append(i)
+	while i < limit:
+		print "At the top i is %d" %i
+		numbers.append(i)
 
-	i=i+1
-	print "Numbers now: ", numbers
-	print "At the bottom i is %d" %i
+		i=i+1
+		print "Numbers now: ", numbers
+		print "At the bottom i is %d" %i
 
-print "The numbers: "
+	print "The numbers: "
 
-for num in numbers:
-	print num
+	for num in numbers:
+		print num
+
+a = raw_input("Input a positive number:")
+whileTest(int(a))
